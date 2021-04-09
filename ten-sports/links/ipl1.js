b8efd4a5b6de08e9cd5fd94f8f937201cbba0df9
Bilal Hassan <bilal.hassan7071@gmail.com>
2021-04-09 23:38:55 +0500

    Update ipl1.js

--- ten-sports/links/ipl1.js
+++ ten-sports/links/ipl1.js
@@ -1,6 +1,6 @@
 
  jwplayer("vplayer").setup({
-    sources: [{file:"http://stream.tvtap.live:8081/live/skysports-cricket1.stream/chunks.m3u8"}],
+    sources: [{file:"http://stream.tvtap.live:8081/live/skysports-cricket1.stream/chunks"}],
     //hlslabels:{
     //       "1440":"1080p",
     //       "900":"720p",
