2325663ebeaf873fe4e5a149fa5f7a490a402a4b
Bilal Hassan <bilal.hassan7071@gmail.com>
2021-04-24 17:57:41 +0500

    Update ptv1.js

--- ten-sports/links/ptv1.js
+++ ten-sports/links/ptv1.js
@@ -1,6 +1,6 @@
 
  jwplayer("vplayer").setup({
-    sources: [{file:"http://35.244.55.13/ptv/index.m3u8"}],
+    sources: [{file:"http://ptva1.tk/ptv/index.m3u8"}],
     //hlslabels:{
     //       "1440":"1080p",
     //       "900":"720p",
