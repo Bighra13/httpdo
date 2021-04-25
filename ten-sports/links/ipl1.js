d41ab85d2b8d2a11216c923256406994639a731d
Bilal Hassan <bilal.hassan7071@gmail.com>
2021-04-25 15:04:17 +0500

    Update ipl1.js

--- ten-sports/links/ipl1.js
+++ ten-sports/links/ipl1.js
@@ -1,6 +1,6 @@
 
  jwplayer("vplayer").setup({
-    sources: [{file:"http://ipla1.tk/ipl/index.m3u8"}],
+    sources: [{file:"http://cdn2.niazitv.pk:8080/IPL-Live-3/index.m3u8"}],
     //hlslabels:{
     //       "1440":"1080p",
     //       "900":"720p",
