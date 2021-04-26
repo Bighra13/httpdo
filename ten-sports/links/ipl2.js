5e92e47df74ee1f2107cb70b866df44d0a0afd93
Bilal Hassan <bilal.hassan7071@gmail.com>
2021-04-26 15:42:46 +0500

    Update ipl2.js

--- ten-sports/links/ipl2.js
+++ ten-sports/links/ipl2.js
@@ -1,6 +1,6 @@
 
  jwplayer("vplayer").setup({
-    sources: [{file:"http://35.244.55.13/ipl/index.m3u8"}],
+    sources: [{file:"http://stream.tvtap.live:8081/live/skysports-cricket1.stream/playlist.m3u8"}],
     //hlslabels:{
     //       "1440":"1080p",
     //       "900":"720p",
